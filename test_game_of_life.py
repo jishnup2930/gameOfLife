5157ba07022559cf9a25a34e3fc7dba561c88c0d
jishnu <givemeakiss111@gmail.com>
2023-11-08 11:17:50 +0530

    Green:Added test case to check one column alive grid

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -15,3 +15,10 @@ def test_display_board_one_alive():
     assert op == """...
 .*.
 ..."""
+
+def test_one_column_alive_pattern():
+    input_board = {'dimensions':(3,3),
+                  'alive':[(1,0),(1,1),(1,2)]}
+    assert display_board(input_board) == """.*.
+.*.
+.*."""
