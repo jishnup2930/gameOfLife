b6aca2ce4d18ea92c24fe4bb4eb53187625ac408
jishnu <givemeakiss111@gmail.com>
2023-11-08 17:19:37 +0530

    Red:Implemented function to get the next generation grid

--- gameoflife.py
+++ gameoflife.py
@@ -20,8 +20,21 @@ def count_alive_neighbors(grid, x, y):
     count = 0
     row = len(grid)
     col = len(grid[0])
-    for i in range(max(0, x-1), min(row, x+2)):
-        for j in range(max(0, y-1), min(col, y+2)):
-            if (i, j) != (x, y) and grid[i][j] == Alive:
+    for i in range(max(0, x - 1), min(row, x + 2)):
+        for j in range(max(0, y - 1), min(col, y + 2)):
+            if not (i == x and j == y) and grid[i][j] == Alive:
                 count += 1
     return count
+
+def get_next_generation(grid):
+    next_gen = [['.' for _ in range(len(grid[0]))] for _ in range(len(grid))]
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            alive = grid[row][col]
+            live_neighbors = count_neighbors(grid, row, col)
+
+            if (alive and 2 <= live_neighbors <= 3) or (not alive and live_neighbors == 3):
+                next_gen[row][col] = '*'
+
+    return next_gen
