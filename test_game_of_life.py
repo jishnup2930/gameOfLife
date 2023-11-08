817ac550fcd129e3879d36ff913186259b4fd051
jishnu <givemeakiss111@gmail.com>
2023-11-08 11:15:01 +0530

    Green:Added to display one alive in grid

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,4 +1,4 @@
-from gameOfLife import *
+from gameoflife import *
 
 def test_diplay_board_all_dead():
     input_board = {"dimensions":(3,3),
@@ -6,4 +6,12 @@ def test_diplay_board_all_dead():
     op = display_board(input_board)
     assert op =="""...
 ...
+..."""
+
+def test_display_board_one_alive():
+    input_board = {'dimensions':(3,3),
+                   'alive':[(1,1)]}
+    op = display_board(input_board)
+    assert op == """...
+.*.
 ..."""
